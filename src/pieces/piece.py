36b1a96a5d8b19da5cf074f8c0578760e290f9f4
AliMousa27 <jafar.myhero@gmail.com>
2024-06-03 15:39:22 +0200

    craeted inital piece abstract class

---
+++ src/pieces/piece.py
@@ -0,0 +1,36 @@
+from abc import ABCMeta, abstractmethod
+from typing import List, Tuple
+from piece_color import Piece_Color
+class Piece(metaclass=ABCMeta):
+  """
+  constructor to create a given piece
+  Args:
+    name: the name of the piece to be instantiated
+    init_position: the i ital position consisting of a tuple of 2 ints, row then column
+    color: Enum to indicate the color
+  """
+  @abstractmethod
+  def __init__(self,name:str,init_position:tuple,color: Piece_Color) -> None:
+    super().__init__()
+    self.name=name
+    self.init_position=init_position
+    self.color = color
+    
+    
+  """
+  method to calculate all possible moves regardless if they are valid or not
+  
+  Return:
+	a list of tuples where the piece can move in terms of rows and columns
+  """
+  @abstractmethod
+  def calc_all_moves(self) -> List[Tuple]:
+    pass
+
+  """
+  method to change the positon of the piece
+  returns nothing
+  """
+  def move(self)-> None: 
+    pass
+
