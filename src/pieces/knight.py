3cecf041eee9aba9c652fa17450c29aeaaffba9b
AliMousa27 <jafar.myhero@gmail.com>
2024-06-03 16:02:51 +0200

    finished inital pieces structure for all piece classes

---
+++ src/pieces/knight.py
@@ -0,0 +1,13 @@
+from piece import Piece
+from piece_color import Piece_Color
+from typing import List, Tuple
+
+class Rook(Piece):
+  def __init__(self,name:str,init_position:tuple,color: Piece_Color) -> None:
+    super().__init__(name,init_position,color)
+  
+  def calc_all_moves(self) -> List[Tuple]:
+    print("d")
+      
+  def move(self) -> None:
+    print("d")
