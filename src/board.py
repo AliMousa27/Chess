7dbf769c5dcd76055a9d994c04dfcbadcecf6823
AliMousa27 <jafar.myhero@gmail.com>
2024-06-07 20:39:13 +0200

    refactored the setup function

--- src/board.py
+++ src/board.py
@@ -9,16 +9,20 @@ from pieces.queen import Queen
 from pieces.bishop import Bishop
 from pieces.pawn import Pawn
 import pygame
-
+#constants
 SCREEN_SIZE = 600
+SIZE= SCREEN_SIZE//8
+GREEN_COLOR = 118,150,86,255
+WHITE_COLOR = 238,238,210,255
 
+#class that has 2 main responsibilites 1. draw and render the board 2. House the pieces on squares to keep track of them
 class Board():
   def __init__(self) -> None:
     pygame.init()
     self.board=[[None]*8 for _ in range(8)]
     self.screen = pygame.display.set_mode((SCREEN_SIZE,SCREEN_SIZE))
     self.clock = pygame.time.Clock()
-    #attribute to hold the images
+    #attribute to hold the image. The key is PieceName_Color and the value is a loaded image by pygame to be rendered later
     self.piece_images  = self.load_piece_images()
     self.board: List[List[Square]] = self.setup()
     
@@ -28,46 +32,46 @@ class Board():
     size = SCREEN_SIZE//8
     #each key is just a col and the value is a pices class over an object. This is done so we construct concrete objects with specifics colors 
     INITAL_PIECES_MAP= {0: Rook, 1: Knight, 2: Bishop, 3: Queen, 4: King, 5:Bishop, 6: Knight, 7:Rook}
+    #draw squares
     for row in range(8):
       for col in range(8):
-        
-        if (row+col)%2==0: pygame.draw.rect(self.screen, pygame.Color(118,150,86,255), [size*col,size*row,size,size])
-        else: pygame.draw.rect(self.screen, pygame.Color(238,238,210,255), [size*col,size*row,size,size])
+        #adding row and col gives the alternating of squares effect
+        if (row+col)%2==0: pygame.draw.rect(self.screen, pygame.Color(GREEN_COLOR), [size*col,size*row,size,size])
+        else: pygame.draw.rect(self.screen, pygame.Color(WHITE_COLOR), [size*col,size*row,size,size])
         
     for row in range(8):
       for col in range(8):
-        
-        
         if row == 0:
-          piece_class: Type[Piece] = INITAL_PIECES_MAP[col]
-          piece = piece_class(piece_class.__name__, (row, col), Piece_Color.BLACK)
-          print(piece.name)
-          self.board[row][col]=Square(row,col,True,piece)
-          
-          piece_image = self.piece_images[f'{piece_class.__name__.lower()}_black']
-          self.screen.blit(piece_image, (size*col, size*row))
+          self.load_important_pieces(Piece_Color.BLACK,row,col,INITAL_PIECES_MAP)
           
         elif row == 1:
-          piece = Pawn("Pawn",(row,col),Piece_Color.BLACK)
-          self.board[row][col]=Square(row,col,True,piece)
-          piece_image = self.piece_images[f'{piece_class.__name__.lower()}_black']
-          self.screen.blit(piece_image, (size*col, size*row))
+          self.load_pawn(Piece_Color.BLACK,row,col)
+          
         elif row == 6:
-          piece = Pawn("Pawn",(row,col),Piece_Color.WHITE)
-          self.board[row][col]=Square(row,col,True,piece)
-          piece_image = self.piece_images[f'{piece_class.__name__.lower()}_white']
-          self.screen.blit(piece_image, (size*col, size*row))
+          self.load_pawn(Piece_Color.WHITE,row,col)
         elif row == 7:
-          piece_class: Type[Piece] = INITAL_PIECES_MAP[col]
-          piece = piece_class(piece_class.__name__, (row, col), Piece_Color.WHITE)
-          self.board[row][col]=Square(row,col,True,piece)
-          piece_image = self.piece_images[f'{piece_class.__name__.lower()}_white']
-          self.screen.blit(piece_image, (size*col, size*row))
+          self.load_important_pieces(Piece_Color.WHITE,row,col,INITAL_PIECES_MAP)
         else:
           self.board[row][col]=Square(row,col,False,None)
+          
     pygame.display.flip()
     return self.board
   
+  def load_pawn(self,color:Piece_Color,row:int,col:int):
+    piece = Pawn("Pawn",(row,col),color)
+    self.board[row][col]=Square(row,col,True,piece)
+    color_str = "white" if color ==Piece_Color.WHITE else "black"
+    piece_image = self.piece_images[f'pawn_{color_str}']
+    self.screen.blit(piece_image, (SIZE*col, SIZE*row))
+    
+  def load_important_pieces(self,color:Piece_Color,row:int,col:int,pieces_class_map):
+    piece_class: Type[Piece] = pieces_class_map[col]
+    piece = piece_class(piece_class.__name__, (row, col), color)
+    self.board[row][col]=Square(row,col,True,piece)
+    color_str = "white" if color ==Piece_Color.WHITE else "black"
+    piece_image = self.piece_images[f'{piece_class.__name__.lower()}_{color_str}']
+    self.screen.blit(piece_image, (SIZE*col, SIZE*row))
+    
   def load_piece_images(self):
       #the size of each image will be divided by 8 to get the scaled down size
       size = SCREEN_SIZE // 8
