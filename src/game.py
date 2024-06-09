e0de2b250042be11651d33f6724f0e78cec3f466
AliMousa27 <jafar.myhero@gmail.com>
2024-06-09 15:55:25 +0200

    moved logic of keeping track highlughted squares to game

--- src/game.py
+++ src/game.py
@@ -1,10 +1,15 @@
 
+from math import e
+from typing import List
 from board import Board
 import pygame
+from pieces.piece import Piece
+from pieces.knight import Knight
 from square import Square
 class Game:
     def __init__(self):
         self.board = Board()
+        self.highlighted_moves=[]
 
     def run(self):
         running = True
@@ -24,11 +29,24 @@ class Game:
         
     def calc_all_moves_for_click(self, square_clicked:Square):
         if square_clicked.occupant:
-          print("trynna highlight all koves")
-          self.board.highlight_moves(square_clicked.occupant.calc_all_moves())
+          all_moves=self.filter_moves(square_clicked.occupant.calc_all_moves(),square_clicked.occupant)
+          self.board.highlight_moves(all_moves,self.highlighted_moves)
         else:
-          print("tryna restore colors")
-          self.board.restore_colors()
+          self.board.restore_colors(self.highlighted_moves)
+          
+    def filter_moves(self,all_moves,piece:Piece):
+      print(all_moves)
+      #if its a knight we can just return all the moves that are not occupied by the same color 
+      #as knight can jump over pieces
+      if isinstance(piece,Knight):
+        nice = []
+        for row, col in all_moves:
+            square: Square = self.board.board[row][col]
+            if not square.occupant or square.occupant.color != piece.color:
+                nice.append((row, col))
+        return nice
+      
+      
 def main():
     game = Game()
     game.run()
