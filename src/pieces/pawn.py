c0d41f160a67cb21ced46de7a0d8e1bf55a50c28
AliMousa27 <jafar.myhero@gmail.com>
2024-06-09 15:00:03 +0200

    added all moves logic to all piece classes

--- src/pieces/pawn.py
+++ src/pieces/pawn.py
@@ -2,12 +2,31 @@ from .piece import Piece
 from .piece_color import Piece_Color
 from typing import List, Tuple
 
+from pieces import piece_color
+
 class Pawn(Piece):
   def __init__(self,name:str,position:tuple,color: Piece_Color,img_size:int) -> None:
     super().__init__(name,position,color,img_size)
   
   def calc_all_moves(self) -> List[Tuple]:
-    print("d")
+    #for a pawn, we will include the en passant moves and filter them out in game logic
+    row,col = self.position
+    directions = [(1,1),(-1,-1),(1,0),(-1,0),(-1,1),(1,-1)]
+    moves=[]
+    for dr, dc in directions:
+      new_row,new_col = dr+row,dc+col
+      
+      in_bounds= 0 <= new_row <= 7 and 0 <= new_col<= 7
+      black_valid_move = in_bounds and new_row>row
+      white_valid_move = in_bounds and new_row<row
+      if self.color==Piece_Color.WHITE and white_valid_move:
+        moves.append((new_row,new_col))
+      elif black_valid_move and self.color==Piece_Color.BLACK:
+        moves.append((new_row,new_col))
+    return moves
+        
+        
+        
       
   def move(self) -> None:
     print("d")
