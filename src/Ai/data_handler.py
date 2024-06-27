43f85f39b6064411576d921931ba97f09256bf07
AliMousa27 <jafar.myhero@gmail.com>
2024-06-27 14:38:07 +0200

    fine tuned the training process and added a learning scheduler

---
+++ src/Ai/data_handler.py
@@ -0,0 +1,36 @@
+import chess
+from torch.utils.data import Dataset
+
+class ChessDataset(Dataset):
+    def __init__(self, path:str):
+        self.games = self.load_games(path)
+    
+    def __len__(self):
+        return len(self.games)
+    #the get item will return a list of the board states along with the move made at each state
+    def __getitem__(self, idx):
+        game = self.games[idx]
+        board = chess.Board()
+        states, moves = [], []
+        for move in game.mainline_moves():
+            states.append(board.fen())
+            moves.append(move)
+            board.push(move)
+        return states, moves
+    
+    def collate_batch(batch):
+        boards, moves = [], []
+        for states, moves_ in batch:
+            boards.extend(states)
+            moves.extend(moves_)
+        return boards, moves
+      
+    def load_games(path,limit=1000):
+        games = []
+        with open(path) as pgn_file:
+            for _ in range(limit):
+                game = chess.pgn.read_game(pgn_file)
+                if game is None:
+                    break
+                games.append(game)
+        return games
