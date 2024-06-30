dfb84060df5f37f149452efc1e9a720f7bfba008
AliMousa27 <jafar.myhero@gmail.com>
2024-07-01 00:03:37 +0200

    found new data and parsed it to json

--- src/Ai/data_handler.py
+++ src/Ai/data_handler.py
@@ -7,13 +7,13 @@ import json
 from stockfish import Stockfish
 
 def pgn_to_json(path: str) -> None:
+    #TODO use environment variable for stockfish path
     stockfish = Stockfish(path=r"C:\Users\Jafar\Downloads\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe")
     with open(path,"r") as pgn_file:
         while True:
             game = chess.pgn.read_game(pgn_file)
             if game is None:
-                with open('games_data.json', 'a') as json_file:
-                    json.dump(game_moves_data, json_file, indent=4)
+                break
             game_moves_data: List[Tuple[str, int]] = []
             board = game.board()
             for move in game.mainline_moves():
@@ -34,23 +34,9 @@ class ChessDataset(Dataset):
     def __getitem__(self, idx: int) -> Tuple[str, int]:
         return self.data[idx]
     
-    def load_data(path: str) -> List[Tuple[str, int]]:
+    def load_data(self,path: str) -> List[Tuple[str, int]]:
         with open(path, 'r') as json_file:
             return json.load(json_file)
 
 
-                
-pgn_to_json(r'C:\Users\Jafar\Desktop\Chess\src\Ai\lichess_db_standard_rated_2017-02.pgn')
-
-
-
-'''import json
-
-# Load the JSON data from the file
-with open('games_data.json', 'r') as json_file:
-    games_data = json.load(json_file)
-
-# Iterate through each game and each move within the game
-for game in games_data:
-    for fen, evaluation in game:
-        print(f"FEN: {fen}, Evaluation: {evaluation}")'''
+            
